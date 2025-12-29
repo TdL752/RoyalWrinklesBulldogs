1c3ae18cc88e9e1471fc465dca37fae3ca48abf0
TdL752 <tylerdamon1@aol.com>
2025-12-29 12:13:23 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+
+app.get("/about", (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/available', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'available.html'));
+});
+
+app.get('/reviews', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'reviews.html'));
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+app.listen(port, () => {
+	console.log('listening on port 3000');
+});
