83e2f9571d3e232b5d93707cd2f549a85985268a
TdL752 <tylerdamon1@aol.com>
2026-02-09 15:47:38 -0500

    formatting change to facilitate local development

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
+app.get('/learn', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'learn.html'));
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+app.get('/available', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'available.html'));
+});
+
 app.listen(port, () => {
 	console.log('listening on port 3000');
 });
